fca1a561c32859403c3c16c13d6011ae2c6b1663
Voltzy9 <arthur.genthial@ynov.com>
2020-02-04 10:25:45 +0100

    postgres and redis

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 'use strict';
 
 var express = require('express');
+var postgres = require('postgres');
+var redis = require('redis');
+const { promisify } = require('util');
+
 var port = 3000;
 var app = express();
+
+const client = redis.createClient(6379, 'redis');
+
+const clientAsync = promisify(client.CLIENT).bind(client);
 
 
 
 app.get('/',(req,res) => {
     res.json({message : "David le bg de la night", method : req.method});
 });
-app.get('/status', (req,res) => {
+
+app.get('/status', async(req,res) => {
+    const clientList = await clientAsync('list');
     res.json({
         status:"ok",
         postgresUptime:String,
-        redisConnectClient:Number
+        redisConnectClient:`${clientList.split('\n').length - 1}`
     })
 });
 
